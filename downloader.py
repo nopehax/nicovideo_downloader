d3d404db8465140f6abaa5595a4ea3ef306277f0
nopehax <57407346+nopehax@users.noreply.github.com>
2022-03-27 18:39:14 +0800

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,4 +1,5 @@
 import requests, os
+
 DIR = "D:\Music\Music\Mafumafu\lost"
 PIECE = 36
 URL = "https://vodedge008.dmc.nico/hlsvod/ht2_nicovideo/nicovideo-sm20359879_46bb460da5e857e2756d9975f81a0eacbcc26bb18bcec2b2d9bf1d2860753a75/1/ts/{}.ts?ht2_nicovideo=6-3Mf1WgerMR_1648369159258.gcov34sdfd_r9e9s9_blhbrkznqkhw"
@@ -11,7 +12,7 @@ def download():
         f.close
 
 def extractAudio():
-    command = 'ffmpeg -i "%s\{0}.ts" -vn -acodec copy "%s\{0}.aac"' % (DIR, DIR)
+    command = 'ffmpeg -i "%s\{0}.ts" -vn -acodec copy "%s\{0}.aac"' % (DIR, DIR)        # -i to specify path, -vn to skip the inclusion of video stream, -acodec copy to copy audio stream without encoding
     os.system("cd 'Downloads'")
     for i in range(1, PIECE+1):
         os.system(command.format(i))
